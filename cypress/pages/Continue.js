1685011c5e34c463111315c497457f867b5e0c96
Anisha Basnet <anisha.basnet@javra.com>
2024-04-02 11:39:12 +0545

    continue with enable/disable

--- cypress/pages/Continue.js
+++ cypress/pages/Continue.js
@@ -1,7 +1,7 @@
 class Continue {
   clickContinueButton() {
     cy.xpath(
-      "//button[contains(@class, 'button-primary') and contains(@title, '') and text()='Continue with ordering']"
+      "//button[contains(@class, 'button-primary') and not(contains(@class, 'disable')) and contains(@title, '') and text()='Continue with ordering']"
     )
       .click()
       .then(() => {
@@ -13,13 +13,10 @@ class Continue {
     cy.xpath(
       "//button[contains(@class, 'button-primary') and contains(@title, '') and text()='Continue with ordering']"
     ).then((button) => {
-      const isButtonDisabled = button.attr("disabled") !== undefined;
+      const isButtonDisabled = button.hasClass("disable");
       cy.log(`Is button disabled? ${isButtonDisabled}`);
 
       if (isButtonDisabled) {
-        // If the button is enabled, directly click it
-        this.clickContinueButton();
-      } else {
         // If the button is disabled, click on the eye icon to enable it and then click the button
         cy.xpath("//div[@data-class='le-color-table']/div[1]/div[2]")
           .click()
@@ -28,6 +25,9 @@ class Continue {
             // After enabling the button, click it
             this.clickContinueButton();
           });
+      } else {
+        // If the button is enabled, directly click it
+        this.clickContinueButton();
       }
     });
   }
