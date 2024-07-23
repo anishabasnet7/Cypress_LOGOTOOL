9647adb29f8eac4251fbb6f952e3a32be47a31ca
Anisha Basnet <anbas061@kathmandu>
2024-07-23 11:41:57 +0545

    Continue Fixed

--- cypress/pages/Continue.js
+++ cypress/pages/Continue.js
@@ -10,42 +10,53 @@ class Continue {
   }
 
   performActions() {
-    cy.xpath(
-      "//button[contains(@class, 'button-primary') and contains(@title, '') and text()='Continue with ordering']"
-    ).then((button) => {
-      const isButtonDisabled = button.hasClass("disable");
-      cy.log(`Is button disabled? ${isButtonDisabled}`);
+    const eyeIconsXPath = "//div[@class='le-wrap-color-field-wrapper ']/div[2]";
 
-      if (isButtonDisabled) {
-        // If the button is disabled, click on the eye icon to enable it and then click the button
-        const eyeIconsXPath =
-          "//div[@class='le-wrap-color-field-wrapper ']/div[2]";
-        cy.xpath(eyeIconsXPath).then((eyeIcons) => {
-          const maxAttempts = eyeIcons.length;
-          cy.log(`Total eye icons: ${maxAttempts}`);
+    const checkAndClick = () => {
+      cy.xpath(
+        "//button[contains(@class, 'button-primary') and contains(@title, '') and text()='Continue with ordering']"
+      ).then((button) => {
+        const isButtonDisabled = button.hasClass("disable");
+        cy.log(`Is button disabled? ${isButtonDisabled}`);
 
-          for (let i = 0; i < maxAttempts; i++) {
-            cy.xpath(eyeIconsXPath).eq(i).click();
-            cy.wait(5000);
-            cy.log(`Clicked eye icon ${i + 1} of ${maxAttempts}`);
-            cy.xpath(
-              "//button[contains(@class, 'button-primary') and contains(@title, '') and text()='Continue with ordering']"
-            ).then((button) => {
-              const isButtonDisabled = button.hasClass("disable");
-              cy.log(`Is button disabled? ${isButtonDisabled}`);
-              if (!isButtonDisabled) {
-                this.clickContinueButton();
-                return false;
+        if (isButtonDisabled) {
+          // If the button is disabled, click on the eye icon to enable it and then click the button
+          cy.xpath(eyeIconsXPath).then((eyeIcons) => {
+            const maxAttempts = eyeIcons.length;
+            cy.log(`Total eye icons: ${maxAttempts}`);
+
+            const clickEyeIcon = (i) => {
+              if (i >= maxAttempts) {
+                return;
               }
-            });
-          }
-        });
-        this.clickContinueButton();
-      } else {
-        // If the button is enabled, directly click it
-        this.clickContinueButton();
-      }
-    });
+
+              cy.xpath(eyeIconsXPath).eq(i).click();
+              cy.wait(5000);
+              cy.log(`Clicked eye icon ${i + 1} of ${maxAttempts}`);
+
+              cy.xpath(
+                "//button[contains(@class, 'button-primary') and contains(@title, '') and text()='Continue with ordering']"
+              ).then((button) => {
+                const isButtonDisabled = button.hasClass("disable");
+                cy.log(`Is button disabled? ${isButtonDisabled}`);
+                if (!isButtonDisabled) {
+                  this.clickContinueButton();
+                } else {
+                  clickEyeIcon(i + 1);
+                }
+              });
+            };
+
+            clickEyeIcon(0);
+          });
+        } else {
+          // If the button is enabled, directly click it
+          this.clickContinueButton();
+        }
+      });
+    };
+
+    checkAndClick();
   }
 }
 
