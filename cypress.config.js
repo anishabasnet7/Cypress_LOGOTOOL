527216abef8e9b7ac089eefde90984fe2d21e62d
Anisha Basnet <anbas061@kathmandu>
2024-07-24 12:55:14 +0545

    Add GitHub Actions workflow for Cypress tests

--- cypress.config.js
+++ cypress.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
     baseUrl: "https://pfconcept.com/en_nl/",
     viewportWidth: 1500,
     viewportHeight: 1000,
-    video: true, // Enable video recording
-    videoCompression: 32, // Adjust video compression level (optional)
+    video: true, 
+    videoCompression: 32,
   },
 });
