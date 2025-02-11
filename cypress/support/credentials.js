ec5ec5976596f470fdd496baf5c60d338b472612
Anisha Basnet <anbas061@kathmandu>
2025-02-11 17:28:26 +0545

    credentials json

---
+++ cypress/support/credentials.js
@@ -0,0 +1,10 @@
+export const credentials ={
+    username: "logotooltest@javra.com",
+    password: "PfConcept2@18!@lt",
+    urls: {
+      madrasBag: "https://www.pfconcept.com/en_nl/madras-140-g-m-cotton-tote-bag-7l-120181.html",
+      heroesShirt: "https://www.pfconcept.com/en_nl/heros-short-sleeve-men-s-t-shirt-38028.html",
+      beagleShort: "https://www.pfconcept.com/en_nl/beagle-short-sleeve-kids-t-shirt-k6554.html"
+    }
+  }
+  
